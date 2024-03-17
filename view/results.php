b4151d426abcf363819e310250ea885cec313863
nikhildhage <nikhil.dhage@gmail.com>
2024-03-17 10:15:30 -0700

    Updated results

---
+++ view/results.php
@@ -0,0 +1,18 @@
+<main class="main-results">
+    <?php 
+        $cards=null;
+        for($i=1; $i<=12; $i++)
+        {
+            $result.=$number*$i;
+            $cards.="<div class='card' tabindex='0'>
+                        <div class='card-front'>
+                            {$number}*{$i}
+                        </div>
+                        <div class='card-back'>
+                            {$result}
+                        </div>
+                    </div>";
+        }
+        echo $cards;
+    ?>
+</main>
