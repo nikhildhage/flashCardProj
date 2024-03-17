aeb67078f14b4b45f9e608df39c2427599db0ca3
nikhildhage <nikhil.dhage@gmail.com>
2024-03-17 10:14:11 -0700

    updated form

--- view/form.php
+++ view/form.php
@@ -1,15 +1,15 @@
 <main class="main-entry">
     <h2 id="enterNumber">Enter Number</h2>
-    <form action='<?php $_SERVER["php_self"];?>' method="GET" class="main-entry__form">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="GET" class="main-entry__form">
         <input 
             type="text" 
             class="main-entry__input" 
             id="num" 
             name="num" 
             maxlength="2" 
-            autofocus 
-            required
+            autofocus
+            required 
         >
-        <button class="main-entry__button">GO!</button>
+        <button type="submit" class="main-entry__button" formmethod="GET">GO!</button>
     </form>
 </main>
