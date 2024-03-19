e6caa5e28f56fed907de2db179af2f3cae82fedf
nikhildhage <nikhil.dhage@gmail.com>
2024-03-19 14:15:43 -0700

    Removed unnecessary comma

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <?php if($number){?>;
+    <?php if($number){?>
             <a href="index.php">Click here to pick another number</a>;
     <?php } else{ ?>
         🚀🚀🚀🚀🚀🚀🚀🚀🚀
