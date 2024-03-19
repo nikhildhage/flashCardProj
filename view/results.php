4f478f62c8bbc039d96aa7585d423feecb821cc8
nikhildhage <nikhil.dhage@gmail.com>
2024-03-19 14:16:23 -0700

    Fixed results of flash cards

--- view/results.php
+++ view/results.php
@@ -3,7 +3,7 @@
         $cards=null;
         for($i=1; $i<=12; $i++)
         {
-            $result.=$number*$i;
+            $result=$number*$i;
             $cards.="<div class='card' tabindex='0'>
                         <div class='card-front'>
                             {$number}*{$i}
