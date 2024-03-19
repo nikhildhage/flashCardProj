cdcd570aab2703012baebfb5b37f02d44194ebf6
nikhildhage <nikhil.dhage@gmail.com>
2024-03-19 14:12:32 -0700

    Changed name of number variable from number to num

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php 
-    $number=filter_input(INPUT_GET, 'number', FILTER_SANITIZE_NUMBER_INT);
+    $number=filter_input(INPUT_GET, 'num', FILTER_SANITIZE_NUMBER_INT);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Multiplication Cards</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <?php include("view/header.php");?>
         <?php
-        echo "Number: " . $number;
             if($number)
             {
                 echo "Results page loaded";
